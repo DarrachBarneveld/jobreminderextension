6d36b8373f4683546eed914d59af7fa9ed842e63
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-03-13 11:23:27 +0000

    first commit

---
+++ background.js
@@ -0,0 +1,79 @@
+// chrome.storage.local.get("jobsCount", function (data) {
+//   let jobsCount = data.jobsCount || 0;
+//   // Check if the count exceeds the limit
+//   if (jobsCount >= 5) {
+//     // Do not create alarm or notifications if the daily limit is reached
+//     return;
+//   }
+//   // Create alarm and notification
+//   createAlarm();
+//   createNotification();
+// });
+
+// chrome.alarms.onAlarm.addListener((alarm) => {
+//   chrome.notifications.create(
+//     "job_hunt",
+//     {
+//       type: "basic",
+//       iconUrl: "/images/jobapplication.jpg",
+//       title: "Daily Goal Not Reached!",
+//       message: "Apply For More Jobs Now!",
+//       silent: false,
+//     },
+//     (notificationId) => {
+//       if (chrome.runtime.lastError) {
+//         console.error(chrome.runtime.lastError.message);
+//       } else {
+//         console.log(`Notification ${notificationId} created.`);
+//       }
+//     }
+//   );
+// });
+
+// chrome.notifications.onClicked.addListener((notificationId) => {
+//   if (notificationId === "job_hunt") {
+//     chrome.tabs.create({ url: "https://www.linkedin.com/jobs/" });
+//   }
+// });
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.time) createAlarm();
+
+//   sendResponse(false);
+// });
+
+// function createAlarm() {
+//   chrome.alarms.create("job_hunt", {
+//     delayInMinutes: 0,
+//     periodInMinutes: 1,
+//   });
+// }
+
+// function createNotification() {
+//   chrome.notifications.create(
+//     "job_hunt",
+//     {
+//       type: "basic",
+//       iconUrl: "/images/jobapplication.jpg",
+//       title: "Daily Goal Not Reached!",
+//       message: "Apply For More Jobs Now!",
+//       silent: false,
+//     },
+//     (notificationId) => {
+//       if (chrome.runtime.lastError) {
+//         console.error(chrome.runtime.lastError.message);
+//       } else {
+//         console.log(`Notification ${notificationId} created.`);
+//       }
+//     }
+//   );
+// }
+
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//   console.log(tab.url);
+//   if (tab.url && !tab.url.includes("linkedin.com/jobs")) {
+//     console.log("Not on LinkedIn Jobs Page");
+
+//     createAlarm();
+//   }
+// });
---
+++ content.js
@@ -0,0 +1,9 @@
+(() => {
+  const fireCounter = async () => {
+    const popupBtn = document.getElementById("popupBtn");
+
+    popupBtn.addEventListener("click", () => {
+      console.log("clicked");
+    });
+  };
+})();
