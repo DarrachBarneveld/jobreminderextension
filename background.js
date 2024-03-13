76e288c521c566f53dbaa5a364413472381bf947
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-03-13 16:36:39 +0000

    Add linked in redirect on tab change

--- background.js
+++ background.js
@@ -1,37 +1,45 @@
 let applications;
 
-(async () => {
+// Fetch applications from storage
+async function fetchApplications() {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(["applications"], (obj) => {
+      resolve(obj["applications"] ? JSON.parse(obj["applications"]) : 0);
+    });
+  });
+}
+
+async function checkApplications() {
   applications = await fetchApplications();
 
   console.log("Total applications:", applications);
 
-  console.log(applications);
-
-  if (applications >= 5) {
+  if (applications < 5) {
+    // If the user has not reached their daily goal, create a notification
     createAlarm();
     createNotification();
   }
+}
+
+(() => {
+  checkApplications();
 })();
 
+// Redirect to LinkedIn Jobs page when notification is clicked
 chrome.notifications.onClicked.addListener((notificationId) => {
   if (notificationId === "job_hunt") {
     chrome.tabs.create({ url: "https://www.linkedin.com/jobs/" });
   }
 });
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.time) createAlarm();
-
-  sendResponse(false);
-});
-
+// Create an alarm to remind the user to apply for more jobs
 function createAlarm() {
   chrome.alarms.create("job_hunt", {
     delayInMinutes: 0,
-    periodInMinutes: 1,
   });
 }
 
+// Create LinkedIn Jobs page notification
 function createNotification() {
   chrome.notifications.create(
     "job_hunt",
@@ -52,24 +60,20 @@ function createNotification() {
   );
 }
 
+// Checks the URL of the tab and creates an alarm if the URL is not a LinkedIn Jobs page
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url && !tab.url.includes("linkedin.com/jobs")) {
+  if (tab.url && !tab.url.includes("linkedin.com/jobs") && applications > 5) {
     createAlarm();
   }
 });
 
-async function fetchApplications() {
-  return new Promise((resolve) => {
-    chrome.storage.sync.get(["applications"], (obj) => {
-      resolve(obj["applications"] ? JSON.parse(obj["applications"]) : 0);
-    });
-  });
-}
-
+// Listerns for tab updates and sends a message to content.js
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   chrome.tabs.sendMessage(tabId, { message: "tab_updated" });
+  checkApplications();
 });
 
+// Listens for messages from content.js and increments the applications count
 chrome.runtime.onMessage.addListener(async function (
   request,
   sender,
@@ -84,36 +88,57 @@ chrome.runtime.onMessage.addListener(async function (
 
     incrementApplicationsAlarm();
   }
-
   sendResponse(() => {
     return false;
   });
 });
 
+// Increments the applications incremented alarm
 function incrementApplicationsAlarm() {
   chrome.alarms.create("applications_incremented", {
     delayInMinutes: 0,
   });
 }
 
+// Listens for the applications incremented alarm and creates a notification
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "applications_incremented") {
-    chrome.notifications.create(
-      "applications_incremented",
-      {
-        type: "basic",
-        iconUrl: "/images/jobapplication.jpg",
-        title: "Application Sent",
-        message: `You have sent ${applications} applications today!`,
-        silent: false,
-      },
-      (notificationId) => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-        } else {
-          console.log(`Notification ${notificationId} created.`);
+    if (applications >= 5) {
+      chrome.notifications.create(
+        "applications_incremented",
+        {
+          type: "basic",
+          iconUrl: "/images/jobapplication.jpg",
+          title: "Goal Reached!",
+          message: `You have sent enough applications today!`,
+          silent: false,
+        },
+        (notificationId) => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+          } else {
+            console.log(`Notification ${notificationId} created.`);
+          }
         }
-      }
-    );
+      );
+    } else {
+      chrome.notifications.create(
+        "applications_incremented",
+        {
+          type: "basic",
+          iconUrl: "/images/jobapplication.jpg",
+          title: "Application Sent",
+          message: `You have sent ${applications} applications today!`,
+          silent: false,
+        },
+        (notificationId) => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+          } else {
+            console.log(`Notification ${notificationId} created.`);
+          }
+        }
+      );
+    }
   }
 });
